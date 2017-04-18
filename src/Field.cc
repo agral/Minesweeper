307de4977a70edde88694329dc4e66e26c727e00
aszczerbiak <szczerbiakadam@gmail.com>
2017-04-18 12:48:27 +0200

    Add a Field class
    Adds the Field class. The Field is used to represent a single cell
    of the minesweeper board.

--- src/Field.cc
+++ src/Field.cc
@@ -8,6 +8,36 @@ Field::Field() :
   // All the work is done in the initializer.
 }
 
+FlagCode Field::flagCode()
+{
+  return _flagCode;
+}
+
+void Field::setFlagCode(FlagCode flagCode)
+{
+  _flagCode = flagCode;
+}
+
+bool Field::isBomb()
+{
+  return _isBomb;
+}
+
+void Field::setIsBomb(bool isBomb)
+{
+  _isBomb = isBomb;
+}
+
+bool Field::isKnown()
+{
+  return _isKnown;
+}
+
+void Field::setIsKnown(bool isKnown)
+{
+  _isKnown = isKnown;
+}
+
 void Field::clear()
 {
   _flagCode = FlagCode::Empty;
--- src/Field.h
+++ src/Field.h
@@ -8,14 +8,69 @@ enum class FlagCode
 class Field
 {
  public:
+  /**
+   * Generates a Field with FlagCode::Empty flag code
+   * and with _isBomb and _isKnown flags set to false.
+   *
+   * \post: Generated Field has:
+   *    - _flagCode set to FlagCode::Empty,
+   *    - _isBomb and _isKnown set to false.
+   */
   Field();
+
+  /**
+   * Gets the value of this Field instance's flag code.
+   *
+   * \return value of _flagCode of this Field instance.
+   */
+  FlagCode flagCode();
+
+  /**
+   * Sets new flag code.
+   *
+   * \post this Field instance has _flagCode set to flagCode.
+   */
+  void setFlagCode(FlagCode flagCode);
+
+  /**
+   * Gets the value of _isBomb flag.
+   *
+   * \return value of _isBomb flag of this Field instance.
+   */
+  bool isBomb();
+
+  /**
+   * Sets the value of _isBomb flag.
+   *
+   * \post this Field instance has _isBomb flag set to isBomb.
+   */
+  void setIsBomb(bool isBomb);
+
+  /**
+   * Gets the value of _isKnown flag.
+   *
+   * \return value of _isKnown flag of this Field instance.
+   */
+  bool isKnown();
+
+  /**
+   * Sets the value of _isKnown flag.
+   *
+   * \post this Field instance has _isKnown flag set to isKnown.
+   */
+  void setIsKnown(bool isKnown);
+
+  /**
+   * Clears a Field so that it has the same state as after its initialization.
+   *
+   * \post This Field instance has:
+   *    - _flagCode set to FlagCode::Empty,
+   *    - _isBomb and _isKnown set to false.
+   */
   void clear();
 
- public:
+ private:
   FlagCode _flagCode;
   bool _isBomb;
   bool _isKnown;
 };
-
-
-
