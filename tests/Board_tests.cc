a52f6f76ab9871773e2d2a82638a79eac3e6c06d
aszczerbiak <szczerbiakadam@gmail.com>
2017-04-19 14:14:11 +0200

    [UnitTest] add first Board tests

---
+++ tests/Board_tests.cc
@@ -0,0 +1,14 @@
+#include "catch.hpp"
+#include "../src/Board.h"
+
+TEST_CASE("Board class tests", "[Board]")
+{
+  SECTION("The constructor properly sets width and height of the board")
+  {
+    const int desired_width = 11;
+    const int desired_height = 12;
+    Board b(desired_width, desired_height);
+    REQUIRE(desired_width == b.width());
+    REQUIRE(desired_height == b.height());
+  }
+}
