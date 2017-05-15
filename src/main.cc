7c88cd3f7c8ce62273638262ec0755db9201bc8f
aszczerbiak <szczerbiakadam@gmail.com>
2017-05-15 12:44:40 +0200

    Add a scoped block to the main() method
    The block defines a clear boundary on the lifetime of the objects
    used by the main() method. The presence of the block fixes the order
    of the cleanup messages (i.e. the "Terminating, bye!" message is now
    reasonably expected to come up last).

--- src/main.cc
+++ src/main.cc
@@ -3,31 +3,34 @@
 
 int main()
 {
-  Log::VERBOSE() << "Starting the main method.";
-  Log::DEBUG() << "Creating the AGE::Engine...";
-  AGE::Engine engine;
-
-  Log::DEBUG() << "Initializing the game engine...";
-  if (!engine.init())
+  // A block defining the lifetime of all the game objects:
   {
-    Log::FATAL() << "Advanced Game Engine failed to initialize. Exiting.";
-    exit(1);
+    Log::VERBOSE() << "Starting the main method.";
+    Log::DEBUG() << "Creating the AGE::Engine...";
+    AGE::Engine engine;
+
+    Log::DEBUG() << "Initializing the game engine...";
+    if (!engine.init())
+    {
+      Log::FATAL() << "Advanced Game Engine failed to initialize. Exiting.";
+      exit(1);
+    }
+
+    Log::DEBUG() << "Game engine initialization complete.";
+    Log::DEBUG() << "Loading the media...";
+    if (!engine.loadMedia())
+    {
+      Log::WARNING() << "Warning: some media files failed to load.";
+    }
+    Log::VERBOSE() << "Media loded.";
+
+    Log::DEBUG() << "Starting the game engine's loop.";
+    engine.startLoop();
+
+    // Destroys the window and quits the SDL system:
+    Log::DEBUG() << "Closing the game engine.";
+    engine.close();
   }
-
-  Log::DEBUG() << "Game engine initialization complete.";
-  Log::DEBUG() << "Loading the media...";
-  if (!engine.loadMedia())
-  {
-    Log::WARNING() << "Warning: some media files failed to load.";
-  }
-  Log::VERBOSE() << "Media loded.";
-
-  Log::DEBUG() << "Starting the game engine's loop.";
-  engine.startLoop();
-
-  // Destroys the window and quits the SDL system:
-  Log::DEBUG() << "Closing the game engine.";
-  engine.close();
 
   Log::VERBOSE() << "Terminating - bye!";
   return 0;
