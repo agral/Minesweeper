7663475df14e0e3ec818d6ef43f8105b2b4da54e
aszczerbiak <szczerbiakadam@gmail.com>
2017-05-15 12:29:47 +0200

    Use Logger utility in main program method.
    Rewrites the code of main() method so that it utilizes Logger class
    exclusively.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,4 @@
 #include "Logger/Logger.h"
-
 #include "AGE/Engine.h"
 
 int main()
@@ -8,26 +7,28 @@ int main()
   Log::DEBUG() << "Creating the AGE::Engine...";
   AGE::Engine engine;
 
-  Log::DEBUG() << "Initializing the AGE::Engine...";
+  Log::DEBUG() << "Initializing the game engine...";
   if (!engine.init())
   {
-    // Logger::instance().log(Logger::CRITICAL,
-        // "Advanced Game Engine failed to initialize. Exiting.");
+    Log::FATAL() << "Advanced Game Engine failed to initialize. Exiting.";
     exit(1);
   }
 
-  // Logger::instance().log(Logger::INFO, "Initialization complete.");
-  // Logger::instance().log(Logger::INFO, "Loading the media...");
+  Log::DEBUG() << "Game engine initialization complete.";
+  Log::DEBUG() << "Loading the media...";
   if (!engine.loadMedia())
   {
-    // Logger::instance().log(Logger::WARNING,
-        // "Warning: some media files failed to load!");
+    Log::WARNING() << "Warning: some media files failed to load.";
   }
-  // Logger::instance().log(Logger::INFO, "Media loded.");
+  Log::VERBOSE() << "Media loded.";
 
+  Log::DEBUG() << "Starting the game engine's loop.";
   engine.startLoop();
 
   // Destroys the window and quits the SDL system:
+  Log::DEBUG() << "Closing the game engine.";
   engine.close();
+
+  Log::VERBOSE() << "Terminating - bye!";
   return 0;
 }
